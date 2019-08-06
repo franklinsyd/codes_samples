00a78a7c09f66a02d1e9f2f5e763d47f018da2a4
Sydney Kasongo <sydneybleuops@gmail.com>
2019-08-06 10:58:46 +0200

    php sample codes

--- php_codes_samples/index.php
+++ php_codes_samples/index.php
@@ -8,31 +8,31 @@ require_once("models/db-settings.php");
 if (!securePage($_SERVER['PHP_SELF'])){die();}
 //$_SESSION['page'] = 'index.php';
 require_once("models/header.php");
-		          if(isUserLoggedIn()) { 
-					  $user_n = $loggedInUser->username; 
-					  $qr = "SELECT `user_level` FROM `foreman_db`.`uc_users` WHERE `uc_users`.`user_name`='$user_n' ";
-					 //Execute query
-					 $query = mysqli_query($mysqli,$qr);
-					 
-					 while($row = mysqli_fetch_array($query)) {
-						 $user_level = $row ['user_level'];
-						}
-				 
-					 if ($user_level==1){
-				
-							header("Location: /umk/umk_foreman/manager_portal.php"); 
-						   
-						 }
-						 
-					 else    {   
-					 header("Location: /umk/umk_foreman/not_allowed.php"); 
-				
-					 $qr ="UPDATE `foreman_db`.`uc_users` SET `login_time` = CURRENT_TIMESTAMP WHERE `uc_users`.`user_name` = '$user_n'";
-					 $query = mysqli_query($mysqli,$qr);			
-					 die();  }	 
+		if(isUserLoggedIn()) { 
+			 $user_n = $loggedInUser->username; 
+			 $qr = "SELECT `user_level` FROM `foreman_db`.`uc_users` WHERE `uc_users`.`user_name`='$user_n' ";
+			 //Execute query
+			 $query = mysqli_query($mysqli,$qr);
+			 
+			 while($row = mysqli_fetch_array($query)) {
+			 $user_level = $row ['user_level'];
+			 }
+			 
+			 if ($user_level==1){
+						
+				header("Location: /umk/umk_foreman/manager_portal.php"); 
+								   
 				}
+								 
+			else{   
+			   header("Location: /umk/umk_foreman/not_allowed.php"); 
+						
+			  $qr ="UPDATE `foreman_db`.`uc_users` SET `login_time` = CURRENT_TIMESTAMP WHERE `uc_users`.`user_name` = '$user_n'";
+			  $query = mysqli_query($mysqli,$qr);			
+			  die();  }	 
+			}
 
-				else{
-			    header("Location: /umk/umk_foreman/login.php"); 
-				}
+		else{
+			header("Location: /umk/umk_foreman/login.php"); 
+		}
 ?>
